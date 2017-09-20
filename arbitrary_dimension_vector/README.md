6be410c835b480cb33a42bdc8986eeee066e15cc
2014CAIS01 <zbszx040504@126.com>
2017-09-20 15:17:49 +0800

    update arbitrary_dimension_vector finish header framework, then fix bugs and add features.

---
+++ arbitrary_dimension_vector/README.md
@@ -0,0 +1,14 @@
+# Arbitrary dimension vector
+## Function
+- Operations of arbitrary dimensional vector.
+
+## TODO
+- [x] initialization
+- [x] copy from a vector
+- [x] assignment
+- [x] destructor
+- [ ] Set the coordinates
+- [ ] Get the dimension
+- [ ] Print the vector like (x,y,...)
+- [ ] Operations
+- [ ] Test program
---
+++ arbitrary_dimension_vector/Vector.h
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+#ifndef VECTOR_H
+#define VECTOR_H
+
+template <typename T>
+class Vector
+{
+public:
+    Vector(int dimension = 0);                                       // initialization
+    Vector(const Vector<T>& vec);                                    // copy from a vector
+    Vector& operator=(const Vector<T>& vec);                         // assignment
+    virtual ~Vector();                                               // destructor
+    void Set();                                                      // Set the coordinates
+    int GetDim() const;                                              // Get the dimension
+    void Print() const;                                              // Print the vector like (x,y,...)
+    friend Vector operator+(const Vector<T>& A, const Vector<T>& B); // addition
+    friend Vector operator-(const Vector<T>& A, const Vector<T>& B); // subtraction
+    friend Vector operator^(const Vector<T>& A, const Vector<T>& B); // cross product
+    friend T operator*(const Vector<T>& A, const Vector<T>& B);      // inner product
+    //  etc...
+private:
+    int n; // dimension
+    T* x;  // coordinates
+};
+
+template <typename T>
+Vector<T>::Vector(int dimension)
+{
+    if (dimension <= 0)
+    {
+        n = 0;
+        x = NULL;
+        return;
+    }
+    x = new T[n = dimension];
+    for (int i = 0; i < n; i++)
+        x[i] = 0;
+}
+
+template <typename T>
+Vector<T>::Vector(const Vector<T>& vec)
+{
+    x = NULL;
+    if (n = vec.n)
+    {
+        x = new T[n];
+        for (int i = 0; i < n; i++)
+            x[i]=vec.x[i];
+    }
+}
+
+template <typename T>
+Vector<T>& Vector<T>::operator=(const Vector<T>& vec)
+{
+    if (&vec == this) return *this;
+    if (x != NULL) delete[] x;
+    x = NULL;
+    if (n = vec.n)
+    {
+        x = new T[n];
+        for (int i = 0; i < n; i++)
+            x[i]=vec.x[i];
+    }
+    return *this;
+}
+
+template <typename T>
+Vector<T>::~Vector()
+{
+    if (x != NULL) delete[] x;
+}
+
+#endif // VECTOR_H
